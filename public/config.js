ee59af1863d720a58cf6ed1696a7fb3e85faeeda
orderly-guoqing <guoqing@orderly.network>
2026-07-24 10:56:35 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -33,7 +33,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_SEO_TWITTER_HANDLE": "",
   "VITE_SEO_THEME_COLOR": "#000000",
   "VITE_SEO_KEYWORDS": "",
-  "VITE_AVAILABLE_LANGUAGES": "en,zh,ja,es,ko,vi,de,fr,ru,id,tr,it,pt,uk,pl,nl,tc",
+  "VITE_AVAILABLE_LANGUAGES": "en,zh,ja,es,ko,vi,de,fr,ru,pt,uk,pl,tc",
   "VITE_USE_CUSTOM_PNL_POSTERS": "false",
   "VITE_CUSTOM_PNL_POSTER_COUNT": "0",
   "VITE_TRADING_VIEW_COLOR_CONFIG": "{\"upColor\":\"#d2659a\",\"downColor\":\"#6ee799\",\"pnlUpColor\":\"#e72113\",\"pnlDownColor\":\"#3c9130\",\"chartBG\":\"#4a4b4b\"}",
